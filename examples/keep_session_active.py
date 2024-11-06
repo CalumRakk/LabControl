071b0789affdcc0eab3aa8fff056a0d9015e2e3d
Leo <leocasti2@gmail.com>
2024-11-06 13:06:23 -0500

    refactor: cambia el script de ejemplo a una clase

--- examples/keep_session_active.py
+++ examples/keep_session_active.py
@@ -9,64 +9,52 @@ import logging
 import utils
 import time
 
-
-# Esperar a que el laboratorio inicie
-def wait_for_lab_to_start(lab: LabAWS):
-    logger.info("Esperando al laboratorio a iniciar")
-    while True:
-        if lab.status != LabStatus.ready:
-            time.sleep(1)
-        break
-
-
-def get_data_from_lab(lab: LabAWS):
-    data = lab.getaws()
-
-    if data["data"]["status"] != LabStatus.ready:
-        lab.start()
-        wait_for_lab_to_start(lab)
-        data = lab.getaws()
-
-    return data
-
-
 logger = logging.getLogger(__name__)
 
-logger.info("El programa se ha iniciado")
-lab = LabAWS()
-data = get_data_from_lab(lab)
+
+class LabController:
+    def __init__(self):
+        self.lab = LabAWS()
+
+    def wait_for_lab_to_start(self):
+        logger.info("Esperando al laboratorio a iniciar")
+        while self.lab.status != LabStatus.ready:
+            time.sleep(1)
+
+    def get_data_from_lab(self):
+        data = self.lab.getaws()
+        if data["data"]["status"] != LabStatus.ready:
+            self.lab.start()
+            self.wait_for_lab_to_start()
+            data = self.lab.getaws()
+        return data
+
+    def get_remaining_session_time(self, data):
+        remaining_time_str = data["data"]["sessions"]["remaining_time"]
+        return int(remaining_time_str.split("(")[-1].split()[0])
+
+    def maintain_session_time(self):
+        logger.info("El programa se ha iniciado")
+        data = self.get_data_from_lab()
+        min_remaining = self.get_remaining_session_time(data)
+        logger.info(utils.get_remaining_session_time(min_remaining))
+
+        if min_remaining > 120:
+            difference_in_sec = (min_remaining - 120) * 60
+            utils.sleep_program(difference_in_sec)
+
+        while True:
+            random_wait_minutes = random.randint(5, 15)
+            logger.info(f"Tiempo random de espera {random_wait_minutes} minutos")
+            utils.sleep_program(random_wait_minutes * 60)
+
+            data = self.get_data_from_lab()
+            min_remaining = self.get_remaining_session_time(data)
+            logger.info(utils.get_remaining_session_time(min_remaining))
+            difference_in_sec = (min_remaining - 120) * 60
+            utils.sleep_program(difference_in_sec)
 
 
-min_remaining = int(
-    data["data"]["sessions"]["remaining_time"].split("(")[-1].split()[0]
-)
-
-logger.info(utils.get_remaining_session_time(min_remaining))
-
-# Si a la session le quedan más de dos horas, saca la diferencia y espera a que llegue a dos horas.
-if min_remaining > 120:
-    difference_in_seg = (min_remaining - 120) * 60
-    utils.sleep_program(difference_in_seg)
-
-data = lab.start()
-data = get_data_from_lab(lab)
-min_remaining = int(
-    data["data"]["sessions"]["remaining_time"].split("(")[-1].split()[0]
-)
-logger.info(utils.get_remaining_session_time(min_remaining))
-difference_in_seg = (min_remaining - 120) * 60
-utils.sleep_program(difference_in_seg)
-
-while True:
-    random_wait_minutes = random.randint(5, 15)
-    logger.info(f"Tiempo random de espera {random_wait_minutes} minutos")
-    utils.sleep_program(random_wait_minutes * 60)
-
-    data = lab.start()
-    data = get_data_from_lab(lab)
-    min_remaining = int(
-        data["data"]["sessions"]["remaining_time"].split("(")[-1].split()[0]
-    )
-    logger.info(utils.get_remaining_session_time(min_remaining))
-    difference_in_seg = (min_remaining - 120) * 60
-    utils.sleep_program(difference_in_seg)
+if __name__ == "__main__":
+    controller = LabController()
+    controller.maintain_session_time()
