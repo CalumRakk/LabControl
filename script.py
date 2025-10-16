f912dfecb3e0352fb96bb4387fbee4ec814da27e
Leo <leocasti2@gmail.com>
2025-10-16 08:44:37 -0500

    feat: mueva el todo el contenido del paquete api al paquete principal

--- script.py
+++ script.py
@@ -1,19 +1,7 @@
-from datetime import datetime, timedelta
-import re
-from typing_extensions import cast
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from labcontrol.api.browser.actions_lab_aws import get_course_id, get_lab_item_id, get_lab_item_id, set_cookies_on_driver, switch_to_iframe
-from labcontrol.api.lab_aws_browser import LabAWSBrowserAPI
-from labcontrol.api.lab_aws_http import LabAWSHttpApi, logger
-from labcontrol.api.browser.driver import DriverManager
-from labcontrol.api.parser import load_netscape_cookies
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver import Chrome
-from lxml import html
-from datetime import timedelta
-import requests
+
+
+from labcontrol.lab_aws_http import LabAWSHttpApi
+from labcontrol.parser import load_netscape_cookies
 
 
 cookies_path= r"C:\Users\Leo\Downloads\awsacademy.instructure.com_cookies.txt"
