0d4ab3cfacf6717bbf2878eaf71fc0c7fb3b9f22
Leo <leocasti2@gmail.com>
2025-10-17 02:59:21 -0500

    fix: corrige la lista de paquetes en setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description="",
     author="Leo",
     author_email="leocasti@gmail.com",
-    packages=find_packages(),
+    packages=["labcontrol","labcontrol.browser"]+find_packages(),
     install_requires=["lxml", "requests", "pydantic", "selenium","webdriver-manager"],
 )
